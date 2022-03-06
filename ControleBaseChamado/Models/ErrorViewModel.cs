9ea7093abf62374d256f51fc2832356000a35e44
thiagotescarorj <59888361+thiagotescarorj@users.noreply.github.com>
2022-03-05 23:32:06 -0300

    Projeto criado

---
+++ ControleBaseChamado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControleBaseChamado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
